5519f865e8dd46a0c2bf3c914cb1b2986979d4af
tyleralves <tyler11alves@gmail.com>
2016-03-31 23:22:59 +1300

    Removed route user from app.js

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname)));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
