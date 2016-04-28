9b851fe805ee36f7539c0f1afe64d2a2df36a107
tyleralves <tyler11alves@gmail.com>
2016-04-28 22:57:03 +1200

    Added animated self portrait to about.ejs/ about.js

--- public/components/about.js
+++ public/components/about.js
@@ -8,5 +8,20 @@ angular.module('about-module', [])
     });
 
 function aboutCtrl($timeout){
-    
+    //Draw name in title
+    var i = 1;
+    function animateSelfSvg(num){
+        var selfImage = document.getElementById('self-image');
+        if(num<5){
+            var selfTracePath = document.getElementById('self-svg-' + num);
+            $timeout(function(){
+                Velocity(selfTracePath, {'stroke-dashoffset': 0, 'fill-opacity': 1}, {duration: 2800});
+                animateSelfSvg(i++);
+            },700);
+        }else{
+            //Velocity(selfTracePath, {'stroke-opacity': 0}, {duration: 5000});
+            Velocity(selfImage, {'opacity':1}, {duration: 3000, display: 'block'});
+        }
+    }
+    animateSelfSvg(1);
 }
